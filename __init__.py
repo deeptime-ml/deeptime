02d2dd446acd7f28da6d229aa6b47815d1c8f75b
marscher <m.scherer@fu-berlin.de>
2015-04-20 19:26:22 +0200

    [coor/doc] minor changes

--- __init__.py
+++ __init__.py
@@ -1,4 +1,3 @@
-
 # Copyright (c) 2015, 2014 Computational Molecular Biology Group, Free University
 # Berlin, 14195 Berlin, Germany.
 # All rights reserved.
@@ -37,7 +36,7 @@ Order parameters
     :toctree: generated/
 
     MDFeaturizer - selects and computes features from MD trajectories
-    CustomFeature -
+    CustomFeature - define arbitrary function to extract features
 
 Reader
 ======
@@ -46,9 +45,16 @@ Reader
     :toctree: generated/
 
     FeatureReader - reads features via featurizer
+    NumPyFileReader - reads numpy files
+    PyCSVReader - reads tabulated ascii files
     DataInMemory - used if data is already available in mem
 
 """
 from .feature_reader import FeatureReader
 from .featurizer import MDFeaturizer, CustomFeature
 from .data_in_memory import DataInMemory
+from .numpy_filereader import NumPyFileReader
+from .py_csv_reader import PyCSVReader
+
+# util func
+from .util.reader_utils import create_file_reader
