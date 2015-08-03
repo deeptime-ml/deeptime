2301978c39bf8ccc264cf50c018addbd51123712
gph82 <guille.perez@fu-berlin.de>
2015-08-03 22:14:41 +0200

    [featurizer.pairs] default excluded neighbrors = 0

--- featurizer.py
+++ featurizer.py
@@ -808,26 +808,26 @@ class MDFeaturizer(object):
         return self.topology.select("mass >= 2")
 
     @staticmethod
-    def pairs(sel, excluded_neighbors=2):
+    def pairs(sel, excluded_neighbors=0):
         """
         Creates all pairs between indexes. Will except closest neighbors up to :py:obj:`excluded_neighbors`
-        (default is to exclude 1-2 and 1-3 and neighbors). The self-pair (i,i) is always excluded
+        The self-pair (i,i) is always excluded
 
         Parameters
         ----------
         sel : ndarray((n), dtype=int)
             array with selected atom indexes
 
-        excluded_neighbors: int, default = 2
+        excluded_neighbors: int, default = 0
             number of neighbors that will be excluded when creating the pairs
 
         Returns
         -------
         sel : ndarray((m,2), dtype=int)
             m x 2 array with all pair indexes between different atoms that are at least :obj:`excluded_neighbors`
-            indexes apart, i.e. if i is the index of an atom, the pairs [i,i-n], [i,i-1], [i,i], [i,i+1], [i,i+n], will
-            not be in :py:obj:`sel` (n=excluded_neighbors). Moreover, the list is non-redundant,
-            i.e. if [i,j] is in sel, then [j,i] is not.
+            indexes apart, i.e. if i is the index of an atom, the pairs [i,i-2], [i,i-1], [i,i], [i,i+1], [i,i+2], will
+            not be in :py:obj:`sel` (n=excluded_neighbors) if :py:obj:`excluded_neighbors` = 2.
+            Moreover, the list is non-redundant,i.e. if [i,j] is in sel, then [j,i] is not.
 
         """
 
