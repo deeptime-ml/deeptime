af7c031d2b6c358f75da4b0b998545c4f155bac9
Paul, Fabian (fapa) <fabian.paul@mpikg.mpg.de>
2016-11-24 14:35:04 +0100

    test new variational

--- __init__.py
+++ __init__.py
@@ -1,5 +1,15 @@
 from __future__ import absolute_import
+__author__ = 'noe'
 
-from .moments import moments_XX, moments_XXXY, moments_block
-from .moments import covar, covars
-from .running_moments import RunningCovar, running_covar
+# import subpackages such that they are available after the main package import
+from . import estimators
+from . import solvers
+
+# direct imports of important functions/classes to-level API
+from .solvers.direct import eig_corr
+from .solvers.direct import sort_by_norm
+from .solvers.eig_qr.eig_qr import eig_qr
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
