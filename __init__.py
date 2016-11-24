842be48a9034735292be8b7e82080bfc24fe5dac
Paul, Fabian (fapa) <fabian.paul@mpikg.mpg.de>
2016-11-24 14:36:49 +0100

    wip

--- __init__.py
+++ __init__.py
@@ -10,6 +10,3 @@ from .solvers.direct import eig_corr
 from .solvers.direct import sort_by_norm
 from .solvers.eig_qr.eig_qr import eig_qr
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
