5fbc2ee634843e0888d030441822add460a6244e
Moritz Hoffmann <clonker@gmail.com>
2017-01-05 10:32:34 +0100

    circlefix

--- conda-recipe/run_test.py
+++ conda-recipe/run_test.py
@@ -10,10 +10,15 @@ cover_pkg = test_pkg
 junit_xml = os.path.join(os.getenv('CIRCLE_TEST_REPORTS', '.'), 'junit.xml')
 
 if os.getenv('CONDA_BUILD', False):
-    print("CONDA_BUILD detected.")
-    pytest_cfg = os.path.join(os.getenv('RECIPE_DIR'), '../..', 'setup.cfg')
+    if os.getenv("CIRCLECI", False):
+        pytest_dir = os.path.join(os.path.expanduser("~"), os.getenv("CIRCLE_PROJECT_REPONAME"))
+        pytest_cfg = os.path.join(pytest_dir, 'setup.cfg')
+    else:
+        pytest_dir = os.path.join(os.getenv("RECIPE_DIR"), "../..")
+        pytest_cfg = os.path.join(pytest_dir, 'setup.cfg')
 else:
     pytest_cfg = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../setup.cfg')
+print("Using pytest config file: %s" % pytest_cfg)
 
 # chdir to an path outside of conda-bld, which is known to persist the build phase
 run_dir = tempfile.mkdtemp()
