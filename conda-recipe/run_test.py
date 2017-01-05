0f0102e39c251faab75959dc9fcfa7b5581d8f39
Martin K. Scherer <m.scherer@fu-berlin.de>
2017-01-05 16:49:27 +0100

    [conda-recipe] if conda-build, then include setup.cfg from installation.

--- conda-recipe/run_test.py
+++ conda-recipe/run_test.py
@@ -3,21 +3,17 @@ import os
 import sys
 import pytest
 import shutil
+import pkg_resources
 
 test_pkg = 'pyemma'
 cover_pkg = test_pkg
 
 junit_xml = os.path.join(os.getenv('CIRCLE_TEST_REPORTS', '.'), 'junit.xml')
-
 if os.getenv('CONDA_BUILD', False):
-    if os.getenv("CIRCLECI", False):
-        pytest_dir = os.path.join(os.path.expanduser("~"), os.getenv("CIRCLE_PROJECT_REPONAME"))
-        pytest_cfg = os.path.join(pytest_dir, 'setup.cfg')
-    else:
-        pytest_dir = os.path.join(os.getenv("RECIPE_DIR"), "../..")
-        pytest_cfg = os.path.join(pytest_dir, 'setup.cfg')
+    pytest_cfg = pkg_resources.resource_filename(test_pkg, 'setup.cfg')
 else:
-    pytest_cfg = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../setup.cfg')
+    pytest_cfg = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../../setup.cfg')
+assert os.path.exists(pytest_cfg), pytest_cfg
 print("Using pytest config file: %s" % pytest_cfg)
 
 # chdir to an path outside of conda-bld, which is known to persist the build phase
