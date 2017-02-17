6a8ace46c9f0b34b0b1e46c0554ebb350212335d
Martin K. Scherer <m.scherer@fu-berlin.de>
2017-02-17 16:32:08 +0100

    [setup/recipe] updated dependencies, test against py36

--- conda-recipe/bld.bat
+++
@@ -1,9 +0,0 @@
-if not defined APPVEYOR (
-    echo not on appveyor
-    "%PYTHON%" setup.py install
-) else ( 
-    echo on appveyor
-    cmd /E:ON /V:ON /C %APPVEYOR_BUILD_FOLDER%\devtools\ci\appveyor\run_with_env.cmd "%PYTHON%" setup.py install
-)
-set build_status=%ERRORLEVEL%
-if %build_status% == 1 exit 1
