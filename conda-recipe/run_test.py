7a5b11cb17458ab24f05c3f038cba6350aa505a9
Martin K. Scherer <marscher@users.noreply.github.com>
2017-03-02 13:51:13 +0100

    Fix coverage reports (#1056)
    [travis|coverage] don't import the package prior testing.

--- conda-recipe/run_test.py
+++ conda-recipe/run_test.py
@@ -1,26 +1,22 @@
-import tempfile
 import os
 import sys
 import pytest
 import shutil
-import pkg_resources
+import tempfile
 
 test_pkg = 'pyemma'
 cover_pkg = test_pkg
 
 junit_xml = os.path.join(os.getenv('CIRCLE_TEST_REPORTS', '.'), 'junit.xml')
 
-if os.getenv('CONDA_BUILD', False):
-    pytest_cfg = pkg_resources.resource_filename(test_pkg, 'setup.cfg')
-else:
-    pytest_cfg = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../../setup.cfg')
+pytest_cfg = 'setup.cfg'
 
 print("Using pytest config file: %s" % pytest_cfg)
 assert os.path.exists(pytest_cfg), "pytest cfg not found"
-
-# chdir to an path outside of conda-bld, which is known to persist the build phase
-run_dir = tempfile.mkdtemp()
-os.chdir(run_dir)
+tmp = tempfile.mkdtemp()
+shutil.copy(pytest_cfg, tmp)
+os.chdir(tmp)
+print("current cwd:", os.getcwd())
 
 # matplotlib headless backend
 with open('matplotlibrc', 'w') as fh:
@@ -32,7 +28,7 @@ pytest_args = ("-v --pyargs {test_pkg} "
                "--doctest-modules "
                #"-n 2 "# -p no:xdist" # disable xdist in favour of coverage plugin
                "--junit-xml={junit_xml} "
-               "-c {pytest_cfg} "
+               "-c {pytest_cfg}"
                .format(test_pkg=test_pkg, cover_pkg=cover_pkg,
                        junit_xml=junit_xml, pytest_cfg=pytest_cfg)
                .split(' '))
