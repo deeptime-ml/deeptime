51a4f4ba2e1dd3f0a966f7bcc51a01ae71a802e4
marscher <m.scherer@fu-berlin.de>
2017-05-23 18:30:42 +0200

    [doc] added conda-recipe to build docs

---
+++ doc-recipe/build.sh
@@ -0,0 +1,14 @@
+rm pyemma -r
+
+pyemma_version=`python -c "import pyemma as e; print(e.version)"` 
+export BUILD_DIR=${PREFIX}/${pyemma_version}
+
+cd doc
+pip install -r requirements-build-doc.txt
+
+
+make ipython-rst
+make html
+
+# remove the deps from $PREFIX so we have only the docs left.
+pip uninstall -y -r requirements-build-doc.txt
