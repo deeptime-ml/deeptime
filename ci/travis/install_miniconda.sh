eb69c4cda72245dd66ca7f27dc9908ee50841847
marscher <m.scherer@fu-berlin.de>
2017-11-21 16:44:55 +0100

    cache miniconda

--- ci/travis/install_miniconda.sh
+++ ci/travis/install_miniconda.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
+# taken from https://raw.githubusercontent.com/broadinstitute/viral-ngs/master/travis/install-conda.sh
+set -e
 
-# make TARGET overrideable with env
-: ${TARGET:=$HOME/miniconda}
+# the miniconda directory may exist if it has been restored from cache
+if [ -d "$MINICONDA_DIR" ] && [ -e "$MINICONDA_DIR/bin/conda" ]; then
+    echo "Miniconda install already present from cache: $MINICONDA_DIR"
+    export PATH="$MINICONDA_DIR/bin:$PATH"
+    hash -r
+else # if it does not exist, we need to install miniconda
+    rm -rf "$MINICONDA_DIR" # remove the directory in case we have an empty cached directory
 
-function install_miniconda {
-	if [ -d $TARGET ]; then echo "file exists"; return; fi
-	echo "installing miniconda to $TARGET"
-        if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then                                 
-             platform="Linux"
-        elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-             platform="MacOSX"
-        fi     
-	wget http://repo.continuum.io/miniconda/Miniconda3-latest-$platform-x86_64.sh -O mc.sh -o /dev/null
-	bash mc.sh -b -f -p $TARGET
-}
+    if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+        curl -S https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh > miniconda.sh;
+    else
+        curl -S https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh > miniconda.sh;
+    fi
 
-install_miniconda
-export PATH=$TARGET/bin:$PATH
+    bash miniconda.sh -b -p "$MINICONDA_DIR"
+    chown -R "$USER" "$MINICONDA_DIR"
+    export PATH="$MINICONDA_DIR/bin:$PATH"
+    hash -r
+    conda config --set always_yes yes --set changeps1 no
+    conda config --add channels defaults
+    conda config --add channels conda-forge
+    conda install --quiet -y conda
+    conda config --set auto_update_conda false
+fi
+
+conda info -a # for debugging
