505ba05071e0af7ea8625d7a4590f80be1f56ef9
Martin K. Scherer <m.scherer@fu-berlin.de>
2017-12-17 14:43:56 +0100

    [datasource] declare fields

--- _base/datasource.py
+++ _base/datasource.py
@@ -33,6 +33,8 @@ class DataSource(Iterable, TrajectoryRandomAccessible):
     data it holds. The difference to Iterable is that DataSource is specialized for trajectories, whereas the concept
     of trajectories is generally unknown for Iterable.
     """
+    _serialize_version = 0
+    _serialize_fields = ('_is_reader', ) # other private fields are not needed, because they are set by child impl ctors.
 
     def __init__(self, chunksize=None):
         super(DataSource, self).__init__(chunksize=chunksize)
