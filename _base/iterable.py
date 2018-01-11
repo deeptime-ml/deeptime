b3484f4e78fa0a95d15eec6b0fc8ede673dd16b4
marscher <m.scherer@fu-berlin.de>
2018-01-11 17:59:57 +0100

    removed from future imports (we now exclusively use py3)

--- _base/iterable.py
+++ _base/iterable.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import print_function
 from abc import ABCMeta, abstractmethod
 import numpy as np
 
--- util/reader_utils.py
+++ util/reader_utils.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import absolute_import
 
 from numpy import vstack
 import mdtraj as md
