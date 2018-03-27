6fd549920e12bde014fdb2e5a2bb43c5db2d9f10
marscher <m.scherer@fu-berlin.de>
2018-03-27 18:53:35 +0200

    fix imports

--- sktime/data/__init__.py
+++ sktime/data/__init__.py
@@ -19,19 +19,11 @@
 
 r"""
 ===============================================================================
-data - Data and input/output utilities (:mod:`pyemma.coordinates.data`)
+data - Data and input/output utilities (:mod:`sktime.data`)
 ===============================================================================
 
-.. currentmodule: pyemma.coordinates.data
+.. currentmodule: sktime.data
 
-Order parameters
-================
-
-.. autosummary::
-    :toctree: generated/
-
-    MDFeaturizer - selects and computes features from MD trajectories
-    CustomFeature - define arbitrary function to extract features
 
 Reader
 ======
@@ -39,15 +31,12 @@ Reader
 .. autosummary::
     :toctree: generated/
 
-    FeatureReader - reads features via featurizer
     NumPyFileReader - reads numpy files
     PyCSVReader - reads tabulated ascii files
     DataInMemory - used if data is already available in mem
     FragmentedTrajectoryReader
 
 """
-from .feature_reader import FeatureReader
-from .featurization.featurizer import MDFeaturizer, CustomFeature
 from .data_in_memory import DataInMemory
 from .numpy_filereader import NumPyFileReader
 from .py_csv_reader import PyCSVReader
