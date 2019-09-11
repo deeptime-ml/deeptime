493dae2055241660e585c836f8e6a955e927582d
marscher <m.scherer@fu-berlin.de>
2019-09-11 15:10:55 +0200

    [tests] use msm factory

--- tests/base/test_pickling.py
+++ tests/base/test_pickling.py
@@ -1,16 +1,16 @@
 import pickle
 import unittest
+import unittest.mock as mock
 
 import numpy as np
 
-from sktime.markovprocess import MaximumLikelihoodMSM
+from ..markovprocess import factory
 
 
-class TestBase(unittest.TestCase):
+class TestPickling(unittest.TestCase):
 
     def test_pickle_msm(self):
-        msm = MaximumLikelihoodMSM(lagtime=1, dt_traj='4 ps')
-        msm.fit([np.array([0, 1, 2, 0, 1, 0, 1])],)
+        msm = factory.msm_double_well()
         msm_pickle = pickle.dumps(msm)
         assert b"version" in msm_pickle
 
@@ -24,3 +24,29 @@ class TestBase(unittest.TestCase):
         np.testing.assert_equal(model_restored.transition_matrix, model.transition_matrix)
         assert model_restored.lagtime == model_restored.lagtime
         assert model.count_model.dt_traj == model_restored.count_model.dt_traj
+
+    def test_pickle_bmsm(self):
+        msm = factory.bmsm_double_well(nsamples=10)
+        msm_pickle = pickle.dumps(msm)
+        assert b"version" in msm_pickle
+
+        from numpy.testing import assert_no_warnings
+        msm_restored = assert_no_warnings(pickle.loads, msm_pickle)
+
+        # test that we can predict with the restored decision tree classifier
+        model = msm.fetch_model()
+        model_restored = msm_restored.fetch_model()
+
+        np.testing.assert_equal(model_restored.prior.transition_matrix, model.prior.transition_matrix)
+        assert model_restored.prior.lagtime == model_restored.prior.lagtime
+        assert model.prior.count_model.dt_traj == model_restored.prior.count_model.dt_traj
+
+    def test_old_version_raise_warning(self):
+        """ ensures that a user warning is displayed, when restoring an object stored with an old version.
+
+        """
+        msm = factory.msm_double_well()
+        pickled = pickle.dumps(msm)
+        # now simulate a newer version
+        with mock.patch('sktime.__version__', '99+brand-new'), np.testing.assert_warns(UserWarning, ):
+            pickle.loads(pickled)
