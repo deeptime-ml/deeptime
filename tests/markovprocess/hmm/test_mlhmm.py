5ebe3dcf9a938071225b8956761189b558e1e4f7
Moritz Hoffmann <clonker@gmail.com>
2020-02-12 11:33:14 +0100

    [markovprocess/hmm] test against reference

--- tests/markovprocess/hmm/test_mlhmm.py
+++ tests/markovprocess/hmm/test_mlhmm.py
@@ -57,12 +57,41 @@ class TestAlgorithmsAgainstReference(unittest.TestCase):
         ])
         np.testing.assert_array_almost_equal(beta_out, ref_beta, decimal=4)
 
+    def test_state_probabilities(self):
+        ref_alpha = np.array([
+            [0.8182, 0.1818],
+            [0.8834, 0.1166],
+            [0.1907, 0.8093],
+            [0.7308, 0.2692],
+            [0.8673, 0.1327]
+        ])
+        ref_beta = np.array([
+            [0.5923, 0.4077],
+            [0.3763, 0.6237],
+            [0.6533, 0.3467],
+            [0.6273, 0.3727],
+            [.5, .5]
+        ])
+        gamma = np.zeros((len(self.dtraj), self.transition_probabilities.shape[0]))
+        _bindings.util.state_probabilities(ref_alpha, ref_beta, gamma_out=gamma)
+
+        gamma_ref = np.array([
+            [0.8673, 0.1327],
+            [0.8204, 0.1796],
+            [0.3075, 0.6925],
+            [0.8204, 0.1796],
+            [0.8673, 0.1327]
+        ])
+        np.testing.assert_array_almost_equal(gamma, gamma_ref, decimal=4)
+
     def test_viterbi(self):
         path = viterbi(self.transition_probabilities, self.state_probabilities, np.array([0.5, 0.5]))
         np.testing.assert_array_equal(path, self.dtraj)
 
+
 class TestMLHMM(unittest.TestCase):
     pass
+
 
 if __name__ == '__main__':
     unittest.main()
