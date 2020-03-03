c240b95bdf988d2350d18fcb875e204cff4cb845
Moritz Hoffmann <clonker@gmail.com>
2020-03-03 18:25:46 +0100

    [datasets/markovprocess/hmm] docs (wip)

--- sktime/markovprocess/hmm/__init__.py
+++ sktime/markovprocess/hmm/__init__.py
@@ -1,3 +1,45 @@
+r"""
+.. currentmodule: sktime.markovprocess.hmm
+
+Output models
+-------------
+
+.. autosummary::
+    :toctree: generated/
+
+    DiscreteOutputModel
+    GaussianOutputModel
+
+
+Maximum-likelihood estimation of HMMs
+-------------------------------------
+
+Making initial guesses:
+
+.. autosummary::
+    :toctree: generated/
+
+    initial_guess_discrete_from_data
+    initial_guess_discrete_from_msm
+
+Estimation and resulting model:
+
+.. autosummary::
+    :toctree: generated/
+
+    MaximumLikelihoodHMSM
+    HiddenMarkovStateModel
+
+Bayesian hidden markov state models
+-----------------------------------
+
+.. autosummary::
+    :toctree: generated/
+
+    BayesianHMSM
+    BayesianHMMPosterior
+"""
+
 from .hmm import HiddenMarkovStateModel
 from .maximum_likelihood_hmm import MaximumLikelihoodHMSM, initial_guess_discrete_from_data, \
     initial_guess_discrete_from_msm
