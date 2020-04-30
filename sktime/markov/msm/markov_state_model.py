cf84173af202072baf15674017a433c0c102d991
Moritz Hoffmann <clonker@gmail.com>
2020-04-30 16:24:51 +0200

    [docs] tica update

--- sktime/markov/msm/markov_state_model.py
+++ sktime/markov/msm/markov_state_model.py
@@ -920,28 +920,36 @@ class MarkovStateModel(Model):
         return ReactiveFlux(A, B, netflux, mu=mu, qminus=qminus, qplus=qplus, gross_flux=grossflux,
                             physical_time=self.count_model.physical_time if self.count_model is not None else '1 step')
 
-    def simulate(self, N, start=None, stop=None, dt=1):
-        """
-        Generates a realization of the Markov Model
+    def simulate(self, n_steps: int, start: Optional[int] = None, stop: Optional[int] = None, dt: int = 1,
+                 seed: int = -1):
+        r"""Generates a realization of the Markov Model.
 
         Parameters
         ----------
-        N : int
+        n_steps : int
             trajectory length in steps of the lag time
         start : int, optional, default = None
             starting state
         stop : int or int-array-like, optional, default = None
             stopping hidden set. If given, the trajectory will be stopped before
             N steps once a hidden state of the stop set is reached
-        dt : int
+        dt : int, default=1
             trajectory will be saved every dt time steps.
             Internally, the dt'th power of P is taken to ensure a more efficient simulation.
-
+        seed : int, default=-1
+            If non-negative, this fixes the seed used for sampling to the provided value so that
+            results are reproducible.
 
         Returns
         -------
         (N/dt,) ndarray
             The state trajectory with length N/dt
+
+        Examples
+        --------
+        >>> msm = MarkovStateModel(transition_matrix=np.array([[.7, .3], [.3, .7]]))
+        >>> trajectory = msm.simulate(n_steps=15)
+        >>> print(trajectory)
         """
         from .._markov_bindings import simulation as sim
         if start is None:
@@ -952,7 +960,7 @@ class MarkovStateModel(Model):
             transition_matrix = self.transition_matrix
         if dt > 1:
             transition_matrix = np.linalg.matrix_power(transition_matrix, dt)
-        return sim.trajectory(N=N, start=start, P=transition_matrix, stop=stop, seed=-1)
+        return sim.trajectory(N=n_steps, start=start, P=transition_matrix, stop=stop, seed=seed)
 
     ################################################################################
     # For general statistics
