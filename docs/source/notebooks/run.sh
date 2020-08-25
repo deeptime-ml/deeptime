8892327ce8bf7a1cdd0e26ece8564a68d705fe7b
Moritz Hoffmann <clonker@gmail.com>
2020-08-25 15:35:02 +0200

    notebooks

--- docs/source/notebooks/run.sh
+++ docs/source/notebooks/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 find . -maxdepth 1 -name \*.ipynb -exec sh -c \
-  'for i do  jupyter nbconvert --to notebook --execute $i --inplace --ExecutePreprocessor.timeout=600 ; done' sh {} \;
+  'for i do  jupyter nbconvert --to notebook --execute $i --inplace --ExecutePreprocessor.timeout=6000 ; done' sh {} \;
