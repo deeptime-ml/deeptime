c8b3fe4e32aa72942f7987d82801cf1bace3d0dc
Moritz Hoffmann <clonker@gmail.com>
2020-09-21 14:20:17 +0200

    cdist in torch gaussian kernel

--- sktime/kernels/kernels_torch.py
+++ sktime/kernels/kernels_torch.py
@@ -10,9 +10,22 @@ TensorOrArray = Union[np.ndarray, torch.Tensor]
 
 class TorchGaussianKernel(GaussianKernel):
 
+    @staticmethod
+    def cdist(x1, x2):
+        x1_norm = x1.pow(2).sum(dim=-1, keepdim=True)
+        x2_norm = x2.pow(2).sum(dim=-1, keepdim=True)
+        res = torch.addmm(
+            x2_norm.t(),
+            x1,
+            x2.t(),
+            alpha=-2
+        ).add_(x1_norm).clamp_min_(1e-16)
+        return res
+
     def apply_torch(self, data_1: torch.Tensor, data_2: torch.Tensor):
-        differences = torch.unsqueeze(data_1, -2) - torch.unsqueeze(data_2, -3)
-        distance_matrix = torch.sum(torch.pow(differences, 2), dim=-1)
+        #differences = torch.unsqueeze(data_1, -2) - torch.unsqueeze(data_2, -3)
+        #distance_matrix = torch.sum(torch.pow(differences, 2), dim=-1)
+        distance_matrix = TorchGaussianKernel.cdist(data_1, data_2)
         return torch.exp(-distance_matrix / (2. * self.sigma ** 2))
 
     def apply(self, data_1: TensorOrArray, data_2: TensorOrArray) -> TensorOrArray:
