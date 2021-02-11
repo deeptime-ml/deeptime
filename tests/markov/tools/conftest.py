7fc23064e261c41b9baa31ab8522342a7a122993
Moritz Hoffmann <clonker@gmail.com>
2021-02-11 10:12:31 +0100

    clean up decomposition tests

--- tests/markov/tools/conftest.py
+++ tests/markov/tools/conftest.py
@@ -8,6 +8,6 @@ def session_setup():
     warnings.filterwarnings('once', category=PendingDeprecationWarning)
 
 
-@pytest.fixture(params=[False, True], ids=lambda x: f"sparse={x}")
+@pytest.fixture(params=[False, True], ids=lambda x: f"{'sparse' if x else 'dense'}")
 def sparse_mode(request):
     yield request.param
