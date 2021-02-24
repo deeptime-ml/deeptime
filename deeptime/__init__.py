5c60b42c95eb0a7df4b252cdc3e5087a60271254
Moritz Hoffmann <moritz.hoffmann91@gmail.com>
2021-02-24 12:41:06 +0100

    fix pip package (#127)

--- deeptime/__init__.py
+++ deeptime/__init__.py
@@ -15,15 +15,18 @@ from . import decomposition
 from . import markov
 
 
-def capi_includes():
+def capi_includes(inc_clustering: bool = False, inc_markov: bool = False, inc_markov_hmm: bool = False,
+                  inc_data: bool = False):
     import os
     import sys
     module_path = sys.modules['deeptime'].__path__[0]
-    includes = [os.path.join(module_path, *rest) for rest in [
-        ('src', 'include'),  # common headers
-        ('clustering', 'include'),  # clustering headers
-        ('markov', '_bindings', 'include'),  # markov module headers
-        ('markov', 'hmm', '_bindings', 'include'),  # hmm headers
-        ('data', 'include')  # data headers
-    ]]
+    includes = [os.path.join(module_path, 'src', 'include')]  # common headers
+    if inc_clustering:
+        includes.append(os.path.join(module_path, 'clustering', 'include'))
+    if inc_markov:
+        includes.append(os.path.join(module_path, 'markov', '_bindings', 'include'))
+    if inc_markov_hmm:
+        includes.append(os.path.join(module_path, 'markov', 'hmm', '_bindings', 'include'))
+    if inc_data:
+        includes.append(os.path.join(module_path, 'data', 'include'))
     return includes
