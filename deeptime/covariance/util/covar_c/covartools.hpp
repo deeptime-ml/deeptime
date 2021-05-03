c1abe8f6b6988980e883faa6c81b7db74ab1a6fe
Moritz Hoffmann <clonker@gmail.com>
2021-05-03 11:52:48 +0200

    f

--- deeptime/covariance/util/covar_c/covartools.hpp
+++ deeptime/covariance/util/covar_c/covartools.hpp
@@ -23,24 +23,21 @@ int _variable_cols(np_array_nfc<bool> &np_cols,
                    const np_array_nfc<dtype> &np_X,
                    float tol=0, std::size_t min_constant=0) {
     // compare first and last row to get constant candidates
-    std::size_t i, j;
-    std::size_t ro;
     std::size_t M = static_cast<std::size_t>(np_X.shape(0)), N = static_cast<std::size_t>(np_X.shape(1));
-    dtype diff;
     std::size_t nconstant = N;  // current number of constant columns
     auto cols = np_cols.mutable_data(0);
     auto X = np_X.data(0);
     // by default all 0 (constant)
-    for (j = 0; j < N; j++)
+    for (std::size_t j = 0; j < N; j++)
         cols[j] = false;
 
     // go through all rows in order to confirm constant candidates
-    for (i = 0; i < M; i++) {
-        ro = i * N;
-        for (j = 0; j < N; j++) {
+    for (std::size_t i = 0; i < M; i++) {
+        auto ro = i * N;
+        for (std::size_t j = 0; j < N; j++) {
             if (! cols[j]) {
                 if constexpr (std::is_floating_point<dtype>::value) {
-                    diff = std::abs(X[j] - X[ro + j]);
+                    auto diff = std::abs(X[j] - X[ro + j]);
                     if (diff > tol) {
                         cols[j] = true;
                         nconstant--;
