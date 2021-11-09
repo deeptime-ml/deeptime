a734295efc13a4206f45a16710cd6f10401bc410
Moritz Hoffmann <clonker@gmail.com>
2021-11-09 13:31:05 +0100

    cov

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,6 @@
+import os
+import tempfile
+
 import nox
 
 PYTHON_VERSIONS = ["3.6", "3.7", "3.8", "3.9", "3.10"]
@@ -6,8 +9,19 @@ PYTHON_VERSIONS = ["3.6", "3.7", "3.8", "3.9", "3.10"]
 @nox.session(python=PYTHON_VERSIONS)
 def tests(session: nox.Session) -> None:
     session.install("-r", "tests/requirements.txt")
-    session.install(".")
-    session.run("pytest", "tests/")
+    session.install(".", '--ignore-installed', '--no-cache-dir', '-vvv')
+    if session.posargs and session.posargs[0] == 'cov':
+        xml_results_dest = os.getenv('SYSTEM_DEFAULTWORKINGDIRECTORY', tempfile.gettempdir())
+        assert os.path.isdir(xml_results_dest), 'no dest dir available'
+        cover_pkg = 'deeptime'
+        junit_xml = os.path.join(xml_results_dest, 'junit.xml')
+        cov_xml = os.path.join(xml_results_dest, 'coverage.xml')
+
+        cov_args = [f'--cov={cover_pkg}', f"--cov-report=xml:{cov_xml}", f"--junit-xml={junit_xml}",
+                    "--cov-config=.coveragerc"]
+    else:
+        cov_args = []
+    session.run("pytest", '-vv', '--doctest-modules', '--durations=20', *cov_args, '--pyargs', "tests/", 'deeptime')
 
 
 @nox.session(reuse_venv=True)
