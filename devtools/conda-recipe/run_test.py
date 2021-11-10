2aeb6741bf84c40707dd1052b618a3b3409916bb
Moritz Hoffmann <clonker@gmail.com>
2021-11-10 17:57:53 +0100

    Nox (#164)
    set up nox and perform majority of CI testing w nox

--- devtools/conda-recipe/run_test.py
+++ devtools/conda-recipe/run_test.py
@@ -1,36 +1,5 @@
-import os
 import sys
-
 import pytest
-import tempfile
-
-cover_pkg = 'deeptime'
-xml_results_dest = os.getenv('SYSTEM_DEFAULTWORKINGDIRECTORY', tempfile.gettempdir())
-assert os.path.isdir(xml_results_dest), 'no dest dir available'
-# where to write junit xml
-junit_xml = os.path.join(xml_results_dest, 'junit.xml')
-cov_xml = os.path.join(xml_results_dest, 'coverage.xml')
-print('junit destination:', junit_xml)
-print('coverage dest:', cov_xml)
-pytest_args = ("-vv "
-               "--cov={cover_pkg} "
-               "--cov-report=xml:{dest_report} "
-               "--doctest-modules "
-               "--junit-xml={junit_xml} "
-               "--durations=20 "
-               "--cov-config {cov_config} "
-               "--pyargs tests/ deeptime"
-               .format(cover_pkg=cover_pkg,
-                       junit_xml=junit_xml,
-                       dest_report=cov_xml,
-                       cov_config=".coveragerc"
-                       )
-               .split(' '))
-
-print("args:", pytest_args)
-print('cwd:', os.getcwd())
-print('content:\n', os.listdir(os.getcwd()))
 
 if __name__ == '__main__':
-    res = pytest.main(pytest_args)
-    sys.exit(res)
+    sys.exit(pytest.main("-vv --pyargs tests/".split(' ')))
