2bfb4221fdc938bb740e9fe60859ce9b33a87cd9
Moritz Hoffmann <clonker@gmail.com>
2021-11-25 11:32:49 +0100

    catch2 unit tests infrastructure (#170)

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,5 @@
 import os
 import tempfile
-
 import nox
 
 PYTHON_VERSIONS = ["3.6", "3.7", "3.8", "3.9", "3.10"]
@@ -8,31 +7,42 @@ PYTHON_VERSIONS = ["3.6", "3.7", "3.8", "3.9", "3.10"]
 
 @nox.session(python=PYTHON_VERSIONS)
 def tests(session: nox.Session) -> None:
-    session.install("-r", "tests/requirements.txt")
-    session.install("-e", ".", '-v', silent=False)
-    if session.posargs and session.posargs[0] == 'cov':
-        session.log("Running with coverage")
-        xml_results_dest = os.getenv('SYSTEM_DEFAULTWORKINGDIRECTORY', tempfile.gettempdir())
-        assert os.path.isdir(xml_results_dest), 'no dest dir available'
-        cover_pkg = 'deeptime'
-        junit_xml = os.path.join(xml_results_dest, 'junit.xml')
-        cov_xml = os.path.join(xml_results_dest, 'coverage.xml')
 
-        cov_args = [f'--cov={cover_pkg}', f"--cov-report=xml:{cov_xml}", f"--junit-xml={junit_xml}",
-                    "--cov-config=.coveragerc"]
+    if 'cpp' in session.posargs:
+        session.install("cmake")
+        session.install("conan")
+        pybind11_module_dir = session.run(*"python -m pybind11 --cmakedir".split(" "), silent=True).strip()
+        session.log(f"Found pybind11 module dir: {pybind11_module_dir}")
+        tmpdir = session.create_tmp()
+        session.run("cmake", "-S", ".", "-B", tmpdir, '-DDEEPTIME_BUILD_CPP_TESTS=ON',
+                    "-Dpybind11_DIR={}".format(pybind11_module_dir), '-DCMAKE_BUILD_TYPE=Release', silent=True)
+        session.run("cmake", "--build", tmpdir, "--target", "run_tests")
     else:
-        session.log("Running without coverage")
-        cov_args = []
+        session.install("-r", "tests/requirements.txt")
+        session.install("-e", ".", '-v', silent=False)
+        if 'cov' in session.posargs:
+            session.log("Running with coverage")
+            xml_results_dest = os.getenv('SYSTEM_DEFAULTWORKINGDIRECTORY', tempfile.gettempdir())
+            assert os.path.isdir(xml_results_dest), 'no dest dir available'
+            cover_pkg = 'deeptime'
+            junit_xml = os.path.join(xml_results_dest, 'junit.xml')
+            cov_xml = os.path.join(xml_results_dest, 'coverage.xml')
 
-    test_dirs = ["tests/"]
-    try:
-        import torch
-        # only run doctests if torch is available
-        test_dirs.append('deeptime')
-    except ImportError:
-        pass
+            cov_args = [f'--cov={cover_pkg}', f"--cov-report=xml:{cov_xml}", f"--junit-xml={junit_xml}",
+                        "--cov-config=.coveragerc"]
+        else:
+            session.log("Running without coverage")
+            cov_args = []
 
-    session.run("pytest", '-vv', '--doctest-modules', '--durations=20', *cov_args, '--pyargs', *test_dirs)
+        test_dirs = ["tests/"]
+        try:
+            import torch
+            # only run doctests if torch is available
+            test_dirs.append('deeptime')
+        except ImportError:
+            pass
+
+        session.run("pytest", '-vv', '--doctest-modules', '--durations=20', *cov_args, '--pyargs', *test_dirs)
 
 
 @nox.session(reuse_venv=True)
