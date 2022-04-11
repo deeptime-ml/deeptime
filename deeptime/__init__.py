9624f525408e053a87bc99a99d859745b8bfd19a
Moritz Hoffmann <clonker@gmail.com>
2022-04-11 10:33:39 +0200

    update versioneer

--- deeptime/__init__.py
+++ deeptime/__init__.py
@@ -1,6 +1,5 @@
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
 
 from . import util
 from . import numeric
