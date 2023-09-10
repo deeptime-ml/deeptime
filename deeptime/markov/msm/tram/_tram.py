a6ac0b93a55d688fe8f3af119680105763366220
Moritz Hoffmann <clonker@gmail.com>
2023-09-10 07:14:58 +0200

    unify callback signature with tqdm (#280)

--- deeptime/markov/msm/tram/_tram.py
+++ deeptime/markov/msm/tram/_tram.py
@@ -280,7 +280,7 @@ class TRAMCallback(callbacks.IterationErrorProgressCallback):
     """
 
     def __init__(self, progress, total, log_likelihoods_list=None, increments=None):
-        super().__init__(progress, total=total, description="Running TRAM estimate")
+        super().__init__(progress, total=total, desc="Running TRAM estimate")
         self.log_likelihoods = log_likelihoods_list
         self.increments = increments
         self.last_increment = 0
