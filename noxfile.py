1f4bd2ab87df3a7bf1fdc0114ed2053759eb2a6b
clonker <1685266+clonker@users.noreply.github.com>
2026-03-29 12:08:18 +0200

    fix nox

--- noxfile.py
+++ noxfile.py
@@ -45,7 +45,7 @@ def tests(session: nox.Session) -> None:
                 n_processes = arg.split('=')[1]
                 session.log(f"Running tests with n={n_processes} jobs.")
                 pytest_args.append(f'--numprocesses={n_processes}')
-        session.install("-e", ".[tests,plotting,units]", '-v', silent=False)
+        session.install(".[tests,plotting,units]", '-v', silent=False)
 
         if 'lldb_torch_setup' in session.posargs:
             session.run("lldb", "--batch", "-o", "run", "-o", "bt", "-o", "c", "--", "python", "-m", "pytest",
@@ -66,7 +66,7 @@ def tests(session: nox.Session) -> None:
 
         test_dirs = [str((Path.cwd() / 'tests').absolute())]  # python tests
         if platform != 'darwin':
-            test_dirs += [str((Path.cwd() / 'deeptime').absolute())]  # doctests
+            test_dirs += ['deeptime']  # doctests via installed package
 
         with session.cd("tests"):
             session.run("python", "-m", "pytest", '-vv', '--doctest-modules', '--durations=20', *pytest_args,
